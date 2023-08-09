4752b6f9bec0de2e13a99ad03d7417785702bd9d
ShamasRajput <shamasrajput18@gmail.com>
2023-08-09 20:08:53 +0500

    Initial commit

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Header(){
+    return(
+        <div>
+            <nav id='nav'>
+                <img src="./Trollface.png" id='nav-img'/>
+                <h2 id="nav-title">Meme Generator</h2> 
+                <h4 id='nav-desc'>React Course Project - 3</h4>   
+            </nav>   
+        </div>
+    )
+
+}
